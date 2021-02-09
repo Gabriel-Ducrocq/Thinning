443670179fa307d22368f7bb346218d660849fdb
Gabriel Ducrocq <gabrielducrocq.maths@gmail.com>
2021-02-09 18:22:47 +0100

    fixing bug

--- SteinDiscrepency.py
+++ SteinDiscrepency.py
@@ -9,20 +9,21 @@ import numpy as np
 
 parser = argparse.ArgumentParser(description="Run stein thinning")
 parser.add_argument("chain_path", help="path of the chain")
+parser.add_argument("gradient_path", help="path of the gradient")
 parser.add_argument("indexes_pah", help="path of the indexes and weights")
 parser.add_argument("output_path", help="path of the output weigths")
 
 
 arguments = parser.parse_args()
 chain = np.genfromtxt(arguments.chain_path, delimiter=",")
-weights = np.genfromtxt(arguments.indexes_path, delimiter=",")
+gradient = np.genfromtxt(arguments.gradient_path, delimiter=",")
 
 indexes_path = arguments.indexes_path
 output_path = arguments.output_path
 indexes = np.load(indexes_path, allow_pickle=True)
 indexes = indexes.item()
 ## Construct the stein kernel use for the computation of the KSD:
-vfk0 = make_imq(chain, chain, pre='med')
+vfk0 = make_imq(chain, gradient, pre='med')
 
 
 if "SMPCOV" in indexes.keys() or "SCLMED" in indexes.keys() or "MED" in indexes.keys():
@@ -30,7 +31,7 @@ if "SMPCOV" in indexes.keys() or "SCLMED" in indexes.keys() or "MED" in indexes.
 
     for method in indexes.keys():
         ## Computing k_stein(x,y)
-        thin_mat = kmat(chain[indexes[method]["indexes"]], chain[indexes[method]["indexes"]], vfk0)
+        thin_mat = kmat(chain[indexes[method]["indexes"]], gradient[indexes[method]["indexes"]], vfk0)
         KSD_thin = np.sqrt(np.mean(thin_mat))
         results[method] = KSD_thin
 
@@ -46,7 +47,7 @@ else:
     omega = np.sum(np.abs(weights))
     for i in range(all_selected):
         selected = all_selected[i, :]
-        thin_mat = kmat(chain[np.abs(selected) == 1], chain[np.abs(selected) == 1], vfk0)
+        thin_mat = kmat(chain[np.abs(selected) == 1], gradient[np.abs(selected) == 1], vfk0)
         result = np.sqrt(np.sum(selected[:, None]*thin_mat*selected[None, :])*(omega/N_KEEP))
         all_KSD.append(result)
 
